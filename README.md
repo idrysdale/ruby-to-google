36e78575d873f635e19ca7f711b483c7fb091c45
Ian Drysdale <ian.drysdale@co-operative.coop>
2016-06-03 17:48:31 +0100

    First commit
    A simple sinatra app, serving up a form that posts to a Google
    spreadsheet

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ruby app to post to a google spreadsheet
+
+## How to use
+
+1. Setup `google-drive-ruby` as defined [here](https://github.com/gimite/google-drive-ruby#how-to-use) 
+
+2. Create a file called `secrets.json` containing your Google spreadsheet id:
+
+   ```
+   {
+     "spreadsheet_id": "1CcXiycr..."
+   }
+   ```
---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'google_drive'
+
+set :port, 8080
+set :static, true
+set :public_folder, 'static'
+set :views, 'views'
+
+get '/' do
+  haml :register_form
+end
+
+post '/register' do
+  email = params[:name]
+  name = params[:email]
+
+  hash = JSON.parse(File.read('./secrets.json'))
+
+  session = GoogleDrive.saved_session('config.json')
+  ws = session.spreadsheet_by_key(hash['spreadsheet_id']).worksheets[0]
+
+  next_empty_row = ws.num_rows + 1
+
+  ws[next_empty_row, 1] = email
+  ws[next_empty_row, 2] = name
+  ws.save
+end
